d5baa8a8fe944f11f1dc9e72d41f4114befb54b7
Martin Cerveny <martin@cerveny.net>
2024-03-05 21:19:48 +0100

    Add current Main test version

--- Console/Program.cs
+++ Console/Program.cs
@@ -10,12 +10,39 @@ namespace ProductManager
     {
         static async Task Main(string[] args)
         {
-            await ApiService.GetAndDisplayUserData();
-            //decimal test = 420.69m;
-            //DatabaseControl.CreateRecord("Test39", test);
-            //DatabaseControl.UpdateRecords(12, 69.420);
-            //DatabaseControl.DeleteRecords(7);
-            //DatabaseControl.ReadRecords();           
+            /*
+            * Read Data from API and print to Console
+            */
+            string apiUrl = "https://jsonplaceholder.typicode.com/users";
+            await ApiService.GetAndDisplayUserData(apiUrl);
+
+            /*
+             * Current Data backup
+             * Data saved into ./products_{timestamp}.txt
+             * 
+             */
+            string timestamp = DateTime.Now.ToString("yyyyMMddHHmm");
+            ProductRW.ExportProducts(timestamp); //Current data backup
+
+           
+            
+
+
+            /*
+             * 
+             * 
+             */
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string sFile = System.IO.Path.Combine(sCurrentDirectory, @"../../../products.txt");
+            if (File.Exists(sFile))
+            {
+                ProductRW.ImportProducts(sFile);                
+            }
+
+
+            
+            
+            DatabaseControl.ReadRecords();           
             
         }
     }
