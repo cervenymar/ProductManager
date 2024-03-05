ccdf4f0c5bfe7566ff8e4b06dac2fb24a165b4a0
Martin Cerveny <martin@cerveny.net>
2024-03-05 21:18:07 +0100

    Add exceptions

--- Console/DatabaseControl.cs
+++ Console/DatabaseControl.cs
@@ -1,6 +1,10 @@
-﻿using ProductManager;
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManager;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,56 +13,143 @@ namespace ProductManager
 {
     class DatabaseControl
     {
-        public static void CreateRecord(string name, decimal price)
+        public static void CreateRecord(int Id, string name, decimal price)
         {
-            using (var db = new ProductContext())
+            try
             {
-                // Create
-                Console.WriteLine($"Adding Product {name} into products...");
-                db.Products.Add(new Product { Name = name, Price = price });
-                db.SaveChanges();
+                using (var db = new ProductContext())
+                {
+                    
+                    var productToCreate = db.Products.FirstOrDefault(p => p.Id == Id);
+                    if (productToCreate == null)
+                    {
+                        // Create
+                        Console.WriteLine($"Adding Product {name} into products...");
+                        db.Products.Add(new Product { Id = Id, Name = name, Price = price });
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product with ID = {Id} already exists. Updating product.");
+                        UpdateRecords(Id, name, price);
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+                Console.WriteLine($"Error creating record: {ex.Message}");
+            }
+            catch (DbEntityValidationException ex)
+            {
+                Console.WriteLine($"DbEntityValidationException: {ex.Message}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"InvalidOperationException: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating record: {ex.Message}");
             }
         }
+
         public static void ReadRecords()
         {
-            using (var db = new ProductContext())
+            try
             {
-                // Read
-                Console.WriteLine("Querying for all products:");
-                var products = db.Products.ToList();
-                foreach (var product in products)
+                using (var db = new ProductContext())
                 {
+                    // Read
+                    Console.WriteLine("Querying for all products:");
+                    var products = db.Products.ToList();
+                    foreach (var product in products)
+                    {                        
+                        Console.WriteLine($"Id: {product.Id}, Name: {product.Name}, Price: {product.Price}");
+                    }
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"InvalidOperationException: {ex.Message}");
+            }
+            catch (EntityException ex)
+            {
+                Console.WriteLine($"EntityException: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading records: {ex.Message}");
+            }
+        }
 
-                    Console.WriteLine($"Id: {product.Id}, Name: {product.Name}, Price: {product.Price}");
-                }
-            }
-        }
-        public static void UpdateRecords(int Id, double price)
+        public static void UpdateRecords(int Id, string name, decimal price)
         {
-            using (var db = new ProductContext())
+            try
             {
-                // Update
-                Console.WriteLine("Updating the price of the first product...");
-                var productToUpdate = db.Products.FirstOrDefault(p => p.Id == Id);
-                if (productToUpdate != null)
+
+                using (var db = new ProductContext())
                 {
-                    productToUpdate.Price = (decimal)price;
-                    db.SaveChanges();
+                    // Update
+                    Console.WriteLine($"Updating the price of Product with Id = {Id}");
+                    var productToUpdate = db.Products.FirstOrDefault(p => p.Id == Id);
+                    if (productToUpdate != null)
+                    {
+                        productToUpdate.Name = name;
+                        productToUpdate.Price = (decimal)price;
+                        db.SaveChanges();
+                    }
                 }
             }
+            catch (DbUpdateException ex)
+            {
+                Console.WriteLine($"DbUpdateException: {ex.Message}");
+            }
+            catch (DbEntityValidationException ex)
+            {
+                Console.WriteLine($"DbEntityValidationException: {ex.Message}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"InvalidOperationException: {ex.Message}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error updating record: {ex.Message}");
+            }
         }
+
         public static void DeleteRecords(int Id)
         {
-            using (var db = new ProductContext())
+            try
             {
-                // Delete
-                Console.WriteLine("Deleting the second product...");
-                var productToDelete = db.Products.FirstOrDefault(p => p.Id == Id);
-                if (productToDelete != null)
+                using (var db = new ProductContext())
                 {
-                    db.Products.Remove(productToDelete);
-                    db.SaveChanges();
+                    // Delete
+                    Console.WriteLine("Deleting the second product...");
+                    var productToDelete = db.Products.FirstOrDefault(p => p.Id == Id);
+                    if (productToDelete != null)
+                    {
+                        db.Products.Remove(productToDelete);
+                        db.SaveChanges();
+                    }
                 }
+            }
+            catch (DbUpdateException ex)
+            {
+                Console.WriteLine($"DbUpdateException: {ex.Message}");
+            }
+            catch (DbEntityValidationException ex)
+            {
+                Console.WriteLine($"DbEntityValidationException: {ex.Message}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"InvalidOperationException: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error deleting record: {ex.Message}");
             }
         }
     }
